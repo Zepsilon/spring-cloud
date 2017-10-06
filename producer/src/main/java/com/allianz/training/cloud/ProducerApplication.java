06b6bcd671f5f46f56a15921464cb9fcf17fac8f
Sedat Çelik <Sedat.Celik@allianz.com.tr>
2017-10-06 10:19:21 +0300

    update

--- producer/src/main/java/com/allianz/training/cloud/ProducerApplication.java
+++ producer/src/main/java/com/allianz/training/cloud/ProducerApplication.java
@@ -3,7 +3,9 @@ package com.allianz.training.cloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
+@RemoteApplicationEventScan
 public class ProducerApplication {
 
 	@Value("${server.port}")
@@ -22,6 +26,11 @@ public class ProducerApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="/msg")
+	public String showMsg() {
+		return "Message: "+ this.msg;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/serverinfo")
